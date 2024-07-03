677d167f904df340f9ad8aff7fe72840f71fe68a
gabrielschrittwieser <gabriel.schrittwieser@edu.fh-joanneum.at>
2024-07-03 14:24:08 +0200

    add TODO methods from IQueue interface to template

--- src/main/java/at/fhj/msd/DrinkQueue.java
+++ src/main/java/at/fhj/msd/DrinkQueue.java
@@ -1,4 +1,33 @@
 package at.fhj.msd;
 
 public class DrinkQueue {
+
+    // Constructor
+    public DrinkQueue() {
+    }
+
+    /*
+    public boolean offer(Drink obj) {
+    }
+     */
+
+    /*
+    public Drink poll() {
+    }
+     */
+
+    /*
+    public Drink remove() {
+    }
+     */
+
+    /*
+    public Drink peek() {
+    }
+     */
+
+    /*
+    public Drink element() {
+    }
+     */
 }
